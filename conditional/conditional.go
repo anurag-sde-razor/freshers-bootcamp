b046c8f5f98ecc15cdb7a06eb8de97c46673c68a
Anurag Aman <anurag.nitm@gmail.com>
2025-02-25 23:58:36 +0530

    Added conditional Folder

---
+++ conditional/conditional.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var age int=9
+
+func main(){
+   if age>18{
+	  fmt.Println("Hey! You are Adult")
+   }else{
+	  fmt.Println("Sorry! you are Minor")
+   }
+}
--- variable/variable.go
+++ variable/variable.go
@@ -2,14 +2,24 @@ package main
 
 import "fmt"
 
-var a int 
-var b,c,d bool
+var a int
+var b, c, d bool
 
-var f,g int = 2,6
+var f, g int = 2, 6
+
+var h float64=float64(10)/3
+
+var i float32=float32(10)/3
 
 
-func main(){
 
-   fmt.Println(a,b,c,d)
-   fmt.Println(f,g)
+func main() {
+	v1:="Anurag Aman"
+	rahul, ischild, price := "brother", true, 30000
+	fmt.Println(a, b, c, d)
+	fmt.Println(f, g)
+	fmt.Println(rahul, ischild, price)
+	fmt.Println("10/3",h)
+	fmt.Println("10/3",i)
+	fmt.Printf("v1 is of type %T\n", v1)
 }
