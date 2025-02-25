0dcbd04f64757bf550ca295ad399db7eecca5773
Anurag Aman <anurag.nitm@gmail.com>
2025-02-26 00:20:25 +0530

    updated conditional.go

--- conditional/conditional.go
+++ conditional/conditional.go
@@ -4,10 +4,26 @@ import "fmt"
 
 var age int=9
 
+func switch1(x int)string{
+   switch x{
+
+    case 1:
+      return "one"
+	case 2:
+	  return "two"
+	default:
+	   return "switch case Not Matched"
+}
+}
+
 func main(){
    if age>18{
 	  fmt.Println("Hey! You are Adult")
    }else{
 	  fmt.Println("Sorry! you are Minor")
    }
+   result:=switch1(2)
+
+   fmt.Println(result)
+   
 }
