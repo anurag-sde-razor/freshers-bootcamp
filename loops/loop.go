9215dec08caeafb7e3736a163b62411f603a5863
Anurag Aman <anurag.nitm@gmail.com>
2025-02-26 14:12:15 +0530

    updated lopp.go

--- loops/loop.go
+++ loops/loop.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
+var sum int=0;
+
+func Add(n int)int {
+   for i:=1;i<=n;i++{
+	sum=sum+i
+   }
+   fmt.Println("sum of n Number start from 1:",sum)
+   return 0
+}
+
 func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println("Hello World")
 	}
-
-	//infinite loop
-	for {
-		fmt.Println("Anurag Aman")
-	}
+   Add(10)
+	
 
 }
