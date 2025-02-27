b48619bd1ad75cf0853b6b900f70c5242616de10
Anurag Aman <anurag.nitm@gmail.com>
2025-02-27 15:55:16 +0530

    user.go added

--- 8user_input/user.go
+++ 8user_input/user.go
@@ -1,3 +1,20 @@
 package main
+
 import "fmt"
 
+func add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println("user-input...")
+	var a, b int
+
+	fmt.Println("Enter Number 1")
+	fmt.Scan(&a)
+	fmt.Println("Enter Number 2")
+	fmt.Scan(&b)
+
+	fmt.Println("Result:", add(a, b))
+
+}
